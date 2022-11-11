d27b40f7b712b2cc949f2dd86ee8b818de34da9d
Jav-P <javierandrespp@hotmail.com>
2022-11-10 21:11:59 -0500

    Zaira

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() {
+    console.log('I am from HeaderComponent constructor()!!');
+   }
+
+   @Output() toggleFooterEvt = new EventEmitter();
+   showFooter = true;
+
+  ngOnInit(): void {
+  }
+  toggleFooter() {
+    this.showFooter = !this.showFooter;
+    this.toggleFooterEvt.emit(this.showFooter);
+  }
+
+}
